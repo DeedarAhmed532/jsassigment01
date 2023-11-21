cedaa05d531b3d913659134755e3510d4b3c42c8
Deedar Ahmed <deedarahmed532@gmail.com>
2023-11-21 06:42:34 +0500

    ok
    ok

---
+++ script.js
@@ -0,0 +1,43 @@
+// Alert : Show text in dialog box
+
+
+var name = "Deedar Ahmed";
+var age = 22;
+var city = "karachi";
+var country = "pakistan";
+
+alert(name);
+alert(age);
+alert(city);
+alert(country);
+
+alert("Aus won the world cup")
+
+alert("passionate coder")
+
+alert("Coding like poetry should be short and concise. ― Santosh Kalwar")
+
+alert("Code is like humor. \n When you have to explain it, its bad. ― Cory House")
+
+alert("Alert is done")
+
+
+
+// prompt : used to display a dialog box that asks the user to input some text 
+
+var name = prompt("Enter your Name", "")
+var lname = prompt("Enter your last name", "")
+var age = prompt("Your Age", " ")
+var gender = prompt("Your Gender", "")
+var email = prompt("Your Email", "")
+var city = prompt("Your City", " ")
+var country = prompt("Your Country", "")
+
+
+console.log(name);
+console.log(lname);
+console.log(age);
+console.log(gender);
+console.log(email);
+console.log(city);
+console.log(country);
